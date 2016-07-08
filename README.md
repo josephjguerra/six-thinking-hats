072806521ad4d54b679136daebcd35a3361e02fe
josephjguerra <joseph.j.guerra@gmail.com>
2016-07-08 08:06:49 -0400

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+html prototype
+
+*Six Thinking Hats*
+by Edward de Bono
+https://amzn.com/0316178314
+
+MLA Content Citation:
+Bono, Edward De. Six Thinking Hats. New York: Back Bay Books, 1999. Print.
---
+++ js/index.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+  //make smooth scrolling affect to section links
+  // $('a').click(function(scroll) {
+  // 	scroll.preventDefault();
+  // 	var link = $(this).attr('href');
+  // 	$('html, body').animate({
+  // 		scrollTop: $(link).offset().top
+  // 	}, 500);
+  // });
+  //function for adding key points in guided session
+  function addBlue() {
+    var point = $('input[name=blueKeyPoint]').val();
+    $('.blueList').append('<div class="point">' + point + '</div>');
+    $('.blueKeyPoint').val('');
+    $('#blueCount').text('140');
+  }
+  $('.addBlue').on('click', function() {
+    addBlue();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addBlue();
+    }
+  });
+
+  function addWhite() {
+    var point = $('input[name=whiteKeyPoint]').val();
+    $('.whiteList').append('<div class="point">' + point + '</div>');
+    $('.whiteKeyPoint').val('');
+    $('#whiteCount').text('140');
+  }
+  $('.addWhite').on('click', function() {
+    addWhite();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addWhite();
+    }
+  });
+
+  function addRed() {
+    var point = $('input[name=redKeyPoint]').val();
+    $('.redList').append('<div class="point">' + point + '</div>');
+    $('.redKeyPoint').val('');
+    $('#redCount').text('140');
+  }
+  $('.addRed').on('click', function() {
+    addRed();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addRed();
+    }
+  });
+
+  function addYellow() {
+    var point = $('input[name=yellowKeyPoint]').val();
+    $('.yellowList').append('<div class="point">' + point + '</div>');
+    $('.yellowKeyPoint').val('');
+    $('#yellowCount').text('140');
+  }
+  $('.addYellow').on('click', function() {
+    addYellow();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addYellow();
+    }
+  });
+
+  function addBlack() {
+    var point = $('input[name=blackKeyPoint]').val();
+    $('.blackList').append('<div class="point">' + point + '</div>');
+    $('.blackKeyPoint').val('');
+    $('#blackCount').text('140');
+  }
+  $('.addBlack').on('click', function() {
+    addBlack();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addBlack();
+    }
+  });
+
+  function addGreen() {
+    var point = $('input[name=greenKeyPoint]').val();
+    $('.greenList').append('<div class="point">' + point + '</div>');
+    $('.greenKeyPoint').val('');
+    $('#greenCount').text('140');
+  }
+  $('.addGreen').on('click', function() {
+    addGreen();
+  });
+  $(document).on('keypress', function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      addGreen();
+    }
+  });
+
+  // counts 
+  function text_counter(input_text, target) {
+    var max = input_text.attr("maxlength");
+    input_text.keyup(function() {
+      target.text(max - this.value.length)
+    });
+  }
+  text_counter($("#problemInput"), $("#problemCount"));
+  text_counter($("#blueInput"), $("#blueCount"));
+  text_counter($("#whiteInput"), $("#whiteCount"));
+  text_counter($("#redInput"), $("#redCount"));
+  text_counter($("#yellowInput"), $("#yellowCount"));
+  text_counter($("#blackInput"), $("#blackCount"));
+  text_counter($("#greenInput"), $("#greenCount"));
+});
